91a66be4ff27dc4ddf5cac4e1236d1676b5bd189
Hyuk Hur <hyukhur@gmail.com>
2018-01-16 00:56:18 +0900

    Support carthage package manager

---
+++ HHUIImageNamed/HHUIImageNamed.h
@@ -0,0 +1,19 @@
+//
+//  HHUIImageNamed.h
+//  HHUIImageNamed
+//
+//  Created by Hyuk Hur on 2018-01-16.
+//  Copyright © 2018 Hyuk Hur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HHUIImageNamed.
+FOUNDATION_EXPORT double HHUIImageNamedVersionNumber;
+
+//! Project version string for HHUIImageNamed.
+FOUNDATION_EXPORT const unsigned char HHUIImageNamedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHUIImageNamed/PublicHeader.h>
+
+
